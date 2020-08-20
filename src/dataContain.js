ab92bc97064ce85bbf5daf2cc679ca722c77e241
Nerdnickk <56470910+Nerdnickk@users.noreply.github.com>
2020-08-20 23:51:07 +0900

    mend

--- src/dataContain.js
+++ src/dataContain.js
@@ -1,43 +1,43 @@
-import main from "./photo/newWave/main.png";
-import menu from "./photo/newWave/menu.png";
-import archiveList from "./photo/newWave/archiveList.png";
-import productList from "./photo/newWave/productList.png";
-import detail from "./photo/newWave/productDetail.png";
+import main from "./photo/newWave/main.jpg";
+import menu from "./photo/newWave/menu.jpg";
+import archiveList from "./photo/newWave/archiveList.jpg";
+import productList from "./photo/newWave/productList.jpg";
+import detail from "./photo/newWave/productDetail.jpg";
 
 // Insta Web
-import login from "./photo/cloninggram/web/login.png";
-import confirm from "./photo/cloninggram/web/loginConfirm.png";
-import signUp from "./photo/cloninggram/web/signUp.png";
-import home from "./photo/cloninggram/web/home.png";
-import profile from "./photo/cloninggram/web/instaProfile.png";
-import profileEdit from "./photo/cloninggram/web/profileEdit.png";
-import follower from "./photo/cloninggram/web/follower.png";
-import messages from "./photo/cloninggram/web/messages.png";
-import message from "./photo/cloninggram/web/message.png";
-import toUser from "./photo/cloninggram/web/toUser.png";
-import notification from "./photo/cloninggram/web/notification.png";
-import searchPost from "./photo/cloninggram/web/searchPost.png";
+import login from "./photo/cloninggram/web/login.jpg";
+import confirm from "./photo/cloninggram/web/loginConfirm.jpg";
+import signUp from "./photo/cloninggram/web/signUp.jpg";
+import home from "./photo/cloninggram/web/home.jpg";
+import profile from "./photo/cloninggram/web/instaProfile.jpg";
+import profileEdit from "./photo/cloninggram/web/profileEdit.jpg";
+import follower from "./photo/cloninggram/web/follower.jpg";
+import messages from "./photo/cloninggram/web/messages.jpg";
+import message from "./photo/cloninggram/web/message.jpg";
+import toUser from "./photo/cloninggram/web/toUser.jpg";
+import notification from "./photo/cloninggram/web/notification.jpg";
+import searchPost from "./photo/cloninggram/web/searchPost.jpg";
 
 // Insta App
 
-import appLogin from "./photo/cloninggram/app/login.png";
-import appSignUp from "./photo/cloninggram/app/signUp.png";
-import appHome from "./photo/cloninggram/app/home.png";
-import appDetail from "./photo/cloninggram/app/postDetail.png";
-import appPostOptions from "./photo/cloninggram/app/postOptions.png";
-import appPostEdit from "./photo/cloninggram/app/postEdit.png";
-import appUploadSingle from "./photo/cloninggram/app/uploadSingle.png";
-import appUploadMultiple from "./photo/cloninggram/app/uploadMultiple.png";
-import appUploadFeed from "./photo/cloninggram/app/uploadFeed.png";
-import appProfile from "./photo/cloninggram/app/profile.png";
-import appProfileEdits from "./photo/cloninggram/app/profileEdits.png";
-import appProfileEdit from "./photo/cloninggram/app/profileEdit.png";
-import appMessage from "./photo/cloninggram/app/message.png";
-import appMessages from "./photo/cloninggram/app/messages.png";
-import appToUser from "./photo/cloninggram/app/toUser.png";
-import appComment from "./photo/cloninggram/app/comment.png";
-import appSearch from "./photo/cloninggram/app/search.png";
-import appNotification from "./photo/cloninggram/app/notification.png";
+import appLogin from "./photo/cloninggram/app/login.jpg";
+import appSignUp from "./photo/cloninggram/app/signUp.jpg";
+import appHome from "./photo/cloninggram/app/home.jpg";
+import appDetail from "./photo/cloninggram/app/postDetail.jpg";
+import appPostOptions from "./photo/cloninggram/app/postOptions.jpg";
+import appPostEdit from "./photo/cloninggram/app/postEdit.jpg";
+import appUploadSingle from "./photo/cloninggram/app/uploadSingle.jpg";
+import appUploadMultiple from "./photo/cloninggram/app/uploadMultiple.jpg";
+import appUploadFeed from "./photo/cloninggram/app/uploadFeed.jpg";
+import appProfile from "./photo/cloninggram/app/profile.jpg";
+import appProfileEdits from "./photo/cloninggram/app/profileEdits.jpg";
+import appProfileEdit from "./photo/cloninggram/app/profileEdit.jpg";
+import appMessage from "./photo/cloninggram/app/message.jpg";
+import appMessages from "./photo/cloninggram/app/messages.jpg";
+import appToUser from "./photo/cloninggram/app/toUser.jpg";
+import appComment from "./photo/cloninggram/app/comment.jpg";
+import appSearch from "./photo/cloninggram/app/search.jpg";
+import appNotification from "./photo/cloninggram/app/notification.jpg";
 
 export const photos = {
 	newWave: [
